320603f28b5df24fb9ed36bd1d58b2369e929e56
Alenso Lopes <alenso.lopes@gmail.com>
2023-12-21 10:43:27 +0100

    refractor: match h file and c file

--- src/subject.c
+++ src/subject.c
@@ -1,7 +1,7 @@
 #include "header/subject.h"
 
-double getAverage(char* name, Subject subject[], int taille){
-	for(int i = 0; i < taille; i++){
+double getAverage(char* name, Subject subject[], int size){
+	for(int i = 0; i < size; i++){
 		if(subject[i].name == name)
 			return subject[i].average;
 	}
