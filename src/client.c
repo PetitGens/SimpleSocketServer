e28b4796125b715e2f09aeae8497321a79ffbbf1
Julien Ait azzouzene <aitazzo221@unicaen.fr>
2023-12-21 13:21:02 +0100

    ✨ feat: working client

--- src/client.c
+++ src/client.c
@@ -6,16 +6,35 @@
 #include "../header/socket.h"
 #include "../header/subject.h"
 
+#define BUF_SIZE 256
+
+void sendMessage(int socket, const char* message){
+    write(socket, message, strlen(message));
+}
+
 void start(int argc, char** argv){
-    if(argc < 3){
-        printf("Ce programme prend 2 arguments en paramètre.\n");
+    if(argc < 4){
+        printf("Ce programme prend 3 arguments en paramètre.\n");
         exit(EXIT_FAILURE);
     }
 
     int port = atoi(argv[2]);
+
+    // Check if port is between 1 and 65535 (2^16 - 1)
+    if(port <= 0 || port > 0xFFFF){
+        printf("Port invalide.\n");
+        exit(EXIT_FAILURE);
+    }
+
     int socket = connectToServer(argv[1], port);
-    char* msg = "Hello World!\n";
-    write(socket, msg, strlen(msg));
+
+    sendMessage(socket, argv[3]);
+
+    char response[BUF_SIZE];
+    read(socket, response, BUF_SIZE);
+
+    printf("Moyenne  : %s.\n", response);
+
     close(socket);
 }
 
